2c8cb57fbe5922d500c8cdee6a9983c2ae59549f
lijiantong <1243095208@qq.com>
2025-04-24 10:57:11 +0800

    feat: sourceMap还原代码

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ErrorStackParser from 'error-stack-parser'
+import { findCodeBySourceMap } from './utils/index'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,13 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.config.errorHandler = (err, vm) => {
+  const errorStack = ErrorStackParser.parse(err as Error)
+  console.log(errorStack, 'errorStack')
+  findCodeBySourceMap(errorStack[0])
+  //   console.error(err, 'error')
+  //   console.error(vm, 'vm')
+}
 
 app.mount('#app')
